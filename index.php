a4db5bb562e71d560ce61f558a0d8cbd0e2d9518
Thomasv07 <Thomas.v07@hotmail.fr>
2021-01-18 09:53:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<title>Table de multiplication</title>
+</head>
+<body>
+<?php
+// $nom = "Vagner";
+// $civilite = "Thomas";
+// echo 'Bonjour '  .$nom. ' ' .$civilite.  ' ! ' ;
+
+
+// echo "<br />" ;
+
+
+// $nb1 = 10  ;
+// $nb2 = 15  ; 
+// $resultat = $nb2+$nb1 ;
+// echo  $resultat ;
+
+
+// echo "<br />" ;
+
+
+// $age =  31 ;
+// if ($age < 18) {
+//     echo 'mineur';
+// } else {
+//     echo ' majeur';
+// }
+
+
+// echo "<br />" ;
+
+
+// $budget = 2236.45 ;
+// $achat = 2236.79 ;
+// $resultat1 = $budget-$achat ;
+// echo ' mon budget - mon achat il me resteras ' ;
+// echo  $resultat1 ;
+// echo "<br />" ;
+// if ($resultat1 >= 0) {
+//     echo 'achat accepté';
+// } else {
+//     echo 'achat refusé';
+// }
+
+// echo "<br />" ;
+
+// $age1 = 31 ;
+// $nationalite = 'française' ;
+// if ($age1 >= 18 && $nationalite=='française') {
+//     echo 'oui';
+// } else {
+//     echo 'non';
+// }
+
+// echo "<br />" ;
+
+// $i = 1 ;
+// while ($i <= 10) {
+//     $resultat2 = $i * 3 ; 
+//     echo $resultat2 ; 
+//     $i++ ;
+// }
+ 
+
+// echo "<br />" ;
+
+
+// for ($j = 1; $j <= 10; $j++) {
+//     $resultat3 = $j * 3;
+//     echo $resultat3;  
+// }
+
+
+// echo "<br />" ;
+
+
+// $j = 1;
+// while ($j <= 10) {
+//     echo "<br />";
+//     $i = 1;
+//     while ($i <=10) {
+//         $resultat = $j * $i;
+//         echo $resultat;
+//         $i++;
+//     }
+//     $j++;
+// }
+
+// for ($j = 1; $j <=10; $j++) {
+//     for ($i = 1; $i <=10; $i++) {
+//         $resultat = $j * $i ;     
+//         echo $resultat;           
+//     }
+//     echo "<br />";
+// }
+?>
+
+<form action=""> 
+
+</form>
+
+</body>
+</html>
+
+
+
+
