7e5325cdf41f5b11be8fa5bd8137b232cef42235
Thomasv07 <76958233+Thomasv07@users.noreply.github.com>
2021-01-19 15:08:48 +0100

    Add files via upload

---
+++ menu/page.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<form action="" method="POST">
+    <label><input type="checkbox" name="test[]" value="test"> test</label>
+    <label><input type="checkbox" name="test[]" value="test"> test 1</label>
+    <label><input type="checkbox" name="test[]" value="test"> test 2</label>
+    <label><input type="checkbox" name="test[]" value="test"> test 3</label>
+    <label><input type="checkbox" name="test[]" value="test"> test 4</label>
+    <input type="submit" value="Valider" />
+</form>
+
+
+
+ <!-- Choisissez votre cr&eacute;neau : </br>
+
+    <form action="" method="post">
+        <select name="nombre">
+            <option value="1"> 1</option>
+            <option value="2"> 2</option>
+            <option value="3"> 3</option>
+            <option value="4"> 4</option>
+            <option value="5"> 5</option>
+            <option value="6"> 6</option>
+            <option value="7"> 7</option>
+            <option value="8"> 8</option>
+            <option value="9"> 9</option>
+            <option value="10"> 10</option>
+        </select> 
+        <input type="submit" value="Valider" />
+    </form> -->
+    <?php
+        //  $nombre=$_POST['nombre'];
+        // for ($j = 1; $j <= 10; $j++) {
+        //         $resultat = $j * $nombre;
+        //         echo 
+        //         echo $resultat; 
+        //     }
+    ?>
+
+</body>
+
+</html>
+ 
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<title>Table de multiplication</title>
+</head>
+<body>
+<?php
+// $nom = "Vagner";
+// $civilite = "Thomas";
+// echo 'Bonjour '  .$nom. ' ' .$civilite.  ' ! ' ;
+
+
+// echo "<br />" ;
+
+
+// $nb1 = 10  ;
+// $nb2 = 15  ; 
+// $resultat = $nb2+$nb1 ;
+// echo  $resultat ;
+
+
+// echo "<br />" ;
+
+
+// $age =  31 ;
+// if ($age < 18) {
+//     echo 'mineur';
+// } else {
+//     echo ' majeur';
+// }
+
+
+// echo "<br />" ;
+
+
+// $budget = 2236.45 ;
+// $achat = 2236.79 ;
+// $resultat1 = $budget-$achat ;
+// echo ' mon budget - mon achat il me resteras ' ;
+// echo  $resultat1 ;
+// echo "<br />" ;
+// if ($resultat1 >= 0) {
+//     echo 'achat accepté';
+// } else {
+//     echo 'achat refusé';
+// }
+
+// echo "<br />" ;
+
+// $age1 = 31 ;
+// $nationalite = 'française' ;
+// if ($age1 >= 18 && $nationalite=='française') {
+//     echo 'oui';
+// } else {
+//     echo 'non';
+// }
+
+// echo "<br />" ;
+
+// $i = 1 ;
+// while ($i <= 10) {
+//     $resultat2 = $i * 3 ; 
+//     echo $resultat2 ; 
+//     $i++ ;
+// }
+ 
+
+// echo "<br />" ;
+
+
+// for ($j = 1; $j <= 10; $j++) {
+//     $resultat3 = $j * 3;
+//     echo $resultat3;  
+// }
+
+
+// echo "<br />" ;
+
+
+// $j = 1;
+// while ($j <= 10) {
+//     echo "<br />";
+//     $i = 1;
+//     while ($i <=10) {
+//         $resultat = $j * $i;
+//         echo $resultat;
+//         $i++;
+//     }
+//     $j++;
+// }
+
+// for ($j = 1; $j <=10; $j++) {
+//     for ($i = 1; $i <=10; $i++) {
+//         $resultat = $j * $i ;     
+//         echo $resultat;           
+//     }
+//     echo "<br />";
+// }
+?>
+
+<form action=""> 
+
+</form>
+
+</body>
+</html>
